6225553888212e1ba21c9497f1032b4f1013a8db
JOHN NASIMBA <nasimba4john@gmail.com>
2020-10-10 14:13:38 +0200

    changed the port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.use(express.static(path.join(__dirname, '/build')))
 app.use(bodyParser.json())
 
